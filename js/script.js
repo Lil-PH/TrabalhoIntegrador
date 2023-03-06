5efc250fa8f63ac91426c18a6c4a739f90bd3529
Lil-PH <pedrohenrique.arnaut019@gmail.com>
2023-03-06 20:08:30 -0300

    add att

--- js/script.js
+++ js/script.js
@@ -8,3 +8,35 @@ function menuShow() {
         document.querySelector('.icon').src = "/TrabalhoIntegrador/image/close_white_36dp.svg";
     }
 }
+
+window.addEventListener('resize', function(event){
+    // Recalcula a largura da imagem quando a tela é redimensionada
+    var imagens = document.getElementsByClassName('conteudo-principal-imagem');
+    for(var i = 0; i < imagens.length; i++){
+      imagens[i].style.width = '100%';
+    }
+
+    var paragrafos = document.getElementsByClassName('conteudo-secundario-paragrafo');
+    for(var i = 0; i < paragrafos.length; i++){
+      paragrafos[i];
+    }
+
+    var titulos = document.getElementsByClassName('conteudo-principal-escrito-titulo');
+    for(var i = 0; i < titulos.length; i++){
+      titulos[i].style.fontSize = '5vw';
+    }
+
+    var titulos = document.getElementsByClassName('conteudo-principal-escrito-subtitulo');
+    for(var i = 0; i < titulos.length; i++){
+      titulos[i].style.fontSize = '5vw';
+    }
+
+    var titulos = document.getElementsByClassName('conteudo-secundario-titulo');
+    for(var i = 0; i < titulos.length; i++){
+      titulos[i].style.fontSize = '5vw';
+    }
+
+  });
+
+  
+  
