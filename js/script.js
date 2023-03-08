f292be1d94628d665e1c59565a7b0e6200b362dc
Pedro Henrique Silva Arnaut <pedrohenrique.arnaut019@gmail.com>
2023-03-08 11:47:33 -0300

    Responsividade 2

--- js/script.js
+++ js/script.js
@@ -1,42 +1,34 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/TrabalhoIntegrador/image/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "/TrabalhoIntegrador/image/close_white_36dp.svg";
-    }
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "../image/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "../image/close_white_36dp.svg";
+  }
 }
 
 window.addEventListener('resize', function(event){
-    // Recalcula a largura da imagem quando a tela é redimensionada
-    var imagens = document.getElementsByClassName('imagem-responsiva');
-    for(var i = 0; i < imagens.length; i++){
-      imagens[i].style.width = '100%';
-    }
 
-    var paragrafos = document.getElementsByClassName('conteudo-secundario-paragrafo');
-    for(var i = 0; i < paragrafos.length; i++){
-      paragrafos[i];
-    }
+  var paragrafos = document.getElementsByClassName('conteudo-secundario-paragrafo');
+  for(var i = 0; i < paragrafos.length; i++){
+    paragrafos[i];
+  }
 
-    var titulos = document.getElementsByClassName('conteudo-principal-escrito-titulo');
-    for(var i = 0; i < titulos.length; i++){
-      titulos[i].style.fontSize = '5vw';
-    }
+  var titulos = document.getElementsByClassName('conteudo-principal-escrito-titulo');
+  for(var i = 0; i < titulos.length; i++){
+    titulos[i].style.fontSize = '5vw';
+  }
 
-    var titulos = document.getElementsByClassName('conteudo-principal-escrito-subtitulo');
-    for(var i = 0; i < titulos.length; i++){
-      titulos[i].style.fontSize = '5vw';
-    }
+  var titulos = document.getElementsByClassName('conteudo-principal-escrito-subtitulo');
+  for(var i = 0; i < titulos.length; i++){
+    titulos[i].style.fontSize = '5vw';
+  }
 
-    var titulos = document.getElementsByClassName('conteudo-secundario-titulo');
-    for(var i = 0; i < titulos.length; i++){
-      titulos[i].style.fontSize = '5vw';
-    }
+  var titulos = document.getElementsByClassName('conteudo-secundario-titulo');
+  for(var i = 0; i < titulos.length; i++){
+    titulos[i].style.fontSize = '5vw';
+  }
 
-  });
-
-  
-  
+});
