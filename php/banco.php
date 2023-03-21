ad7d73bd897fdcf5fada89bcbfc891d80572790b
Lil-PH <pedrohenrique.arnaut019@gmail.com>
2023-03-21 20:14:45 -0300

    Co-authored-by: André Gomes da Silva <andre811922@users.noreply.github.com>

--- php/banco.php
+++ php/banco.php
@@ -1,33 +1,22 @@
 <?php
 // Conectar ao banco de dados
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "dbname";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$host = "babar.db.elephantsql.com";
+$user = "enxywrgz";
+$password = "alRD-xN5PAIkBh93-MM_G1ySYYAMIzSC";
+$dbname = "enxywrgz ";
+$port = "5432";
 
-// Verificar conexão
-if ($conn->connect_error) {
-  die("Falha na conexão: " . $conn->connect_error);
+// Conectando ao banco de dados
+$conexao = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+// Verifica se a conexão foi bem sucedida
+if (!$conexao) {
+    echo "Não foi possível conectar ao banco de dados.";
+    exit;
 }
 
-// Obter dados do formulário
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$telefone = $_POST['telefone'];
-$medico = $_POST['medico'];
-$data = $_POST['data'];
-$horario = $_POST['horario'];
+// Executar consultas SQL aqui...
 
-// Inserir dados no banco de dados
-$sql = "INSERT INTO consultas (nome, email, telefone, medico, data, horario)
-        VALUES ('$nome', '$email', '$telefone', '$medico', '$data', '$horario')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Agendamento de consulta realizado com sucesso!";
-} else {
-  echo "Erro ao agendar consulta: " . $conn->error;
-}
-
-$conn->close();
+// Encerrar a conexão
+pg_close($conexao);
 ?>
