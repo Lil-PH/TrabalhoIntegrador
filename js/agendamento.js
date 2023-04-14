8f33baa713823e05136adc700fc10579c4cff99f
andre811922 <andrezinbere@gmail.com>
2023-04-14 12:54:04 -0300

    Acréscimo em agendamento.html

---
+++ js/agendamento.js
@@ -0,0 +1,9 @@
+function mascara_cpf(){
+    var cpf = document.getElementById('cpf')
+    if (cpf.value.length == 3 || cpf.value.length == 7){
+        cpf.value += "."
+    }else if(cpf.value.length == 11 ){
+        cpf.value += "-"
+
+    }
+}
